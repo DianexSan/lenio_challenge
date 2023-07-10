6ec42b7da5b32c82bd77307c08d43d87f33efcb6
Dianexsan <dianexsandoval@gmail.com>
2023-07-10 19:00:33 -0300

    Resolving step 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useState,useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const MarvelCharacter = () => {
+    const [character,setCharacter] = useState(null);
 
-export default App;
+    useEffect(() => {
+      const getRandomCharacter = async() =>{
+        try {
+          const response = await axios.get('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f680d16d1c5e7b9177e5a9e37378f8ac&hash=fe128e49c557a766af0a11b83249cd6a');
+          const characters = response.data.data.results;
+          const randomIndex = Math.floor(Math.random()*characters.length);
+          const randomCharacter = characters[randomIndex];
+          setCharacter(randomCharacter);
+        } catch(error) {
+          console.log(error);
+        }
+      }
+
+      const savedCharacter = localStorage.getItem('randomCharacter');
+      if (savedCharacter){
+        setCharacter(JSON.parse(savedCharacter));
+        } else {
+          getRandomCharacter();
+        }
+    },[]);
+
+    useEffect(() => {
+      if (character) {
+        localStorage.setItem('randomCharacter', JSON.stringify(character));
+      }
+
+      return () => {
+        localStorage.removeItem('randomCharacter')
+      };
+    }, [character]);
+
+    return (
+      <div>
+        {character ? (
+          <div>
+            <h1>{character.name}</h1>
+            <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt={character.name} />
+          </div>
+        ) : (
+          <p>test</p>
+        )}
+        
+      </div>
+    );
+  };
+
+
+export default MarvelCharacter
+
